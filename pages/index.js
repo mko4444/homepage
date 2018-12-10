07e419e2249f573dc0ff404665240e5d6efe38b2
mkochakian <mattk4@me.com>
2018-12-10 14:01:48 -0500

    ~

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import './index.scss';
+var classNames = require('classnames');
+
+let SITE_LINKS = ["about", "writings", "newsletter", "projects", "bookshelf", "social"],
+    SOCIAL_LINKS = [
+      {
+        "text": "twitter",
+        "url": "https://www.twitter.com/kochakian",
+      }
+    ]
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      titleText: 'Matthew Kochakian'
+    }
+  }
+  populateSiteLinks(linkList) {
+    return linkList.map((link, index) =>
+      <div className="link">{link}</div>
+    )
+  }
+  populateSocialLinks() {
+    return SOCIAL_LINKS.map((link, index) =>
+      <div className={classNames({
+        "lk-con": true,
+        "hide": !this.state.social
+      })}>{link["text"]}</div>
+    )
+  }
   render() {
     return(
-      <div>test</div>
+      <div className="con">
+        <span className="title-text">
+          {this.state.titleText}
+        </span>
+        <div>
+        <div className="dvd"></div>
+        <div className="lk-con">{this.populateSiteLinks(SITE_LINKS)}</div>
+        <div className="dvd"></div>
+        {this.populateSocialLinks()}
+        </div>
+      </div>
     )
   }
 }
